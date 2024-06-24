0f707d4d2244391e228aebaba36576f7419005c6
Jose Ribeiro <joseribeirocsr@gmail.com>
2024-06-24 17:00:00 -0300

    config: configuração das variaveis gerais do sistema.

--- blog/index.php
+++ blog/index.php
@@ -1,11 +1,8 @@
 
 <?php
 
-//declare(strict_types = 1); //Função obriga o php a usar o tipo de dado especifico sem realizar nenhuma alteração do tipo de dado.
 require_once 'sistema/configuracao.php';
 require_once 'Helpers.php';
 
-
-$url = "http://";
-
-var_dump(validarUrlPropia($url));
+echo SITE_NOME;
+echo URL_PRODUCAO;  
--- blog/sistema/configuracao.php
+++ blog/sistema/configuracao.php
@@ -2,3 +2,10 @@
 
 //Arquivos do sistema
 date_default_timezone_set('America/Sao_Paulo');
+
+define('SITE_NOME', 'Unset');
+define('DEFINICAO',  'Unset - Tecnologia em sistemas da informação');
+
+
+define('URL_PRODUCAO', 'https://www.site.com.br');
+define('URL_DESENVOLVIMENTO', 'http://localhost/blog');

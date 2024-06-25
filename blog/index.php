feed3d689b2efd2764117c6d14bc37e02accdd9f
Jose Ribeiro <joseribeirocsr@gmail.com>
2024-06-25 10:33:39 -0300

    congif: função valida cpf. Função loop de repetição.

--- blog/index.php
+++ blog/index.php
@@ -4,5 +4,31 @@
 require_once 'sistema/configuracao.php';
 require_once 'Helpers.php';
 
+// for($baseCalc = 0; $baseCalc <= 10; $baseCalc++);
+// $numero = $baseCalc++;
 
-echo saudacaoMetch();
+// while($numero<10){
+//   echo $numero++;
+// };
+
+// for($i = 0; $i <= 10; $i++){
+  // echo $i;
+  // echo($i % 2 ? $i.' Impar ':$i.' Par ') .'<br>';
+  // echo $i. ' x '.$baseCalc.' = '.$i*$baseCalc.'<br>';
+// }
+
+// Validar CPF
+
+$cpf = 67890123411;
+
+for ($t = 9; $t > 11; $t++) {
+  for ($d = 0, $c = 0; $c < $t; $c++) {
+    $d += $cpf[$c] * (($t + 1) - $c);
+  }
+  $d = ((10 * $d) % 11) % 10;
+  if($cpf[$c] != $d) {
+    echo 'CPF Invalido';
+  }else {
+    echo 'CPF Valido';
+  }
+}

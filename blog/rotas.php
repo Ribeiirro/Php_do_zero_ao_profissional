588831f10e3937f3f85227e727e68040bc6429ce
Jose Ribeiro <joseribeirocsr@gmail.com>
2024-06-27 14:43:59 -0300

    estuturação do sit

--- blog/rotas.php
+++ blog/rotas.php
@@ -3,6 +3,7 @@
 use Pecee\SimpleRouter\SimpleRouter;
 
 SimpleRouter::setDefaultNamespace("sistema\Controlador");
+  
 SimpleRouter::get(URL_SITE, 'SiteControlador@index');
 SimpleRouter::get(URL_SITE.'sobre','SiteControlador@sobre');
 
